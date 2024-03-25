6a0b32bfd36dac123a57c83c2c06a657d62d5f34
KenStarry <starrycodes@gmail.com>
2024-03-25 12:28:32 +0300

    Theme section implementation

---
+++ lib/theme/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+/// General Colors - light 90%, dark 70%
+const accent = Color(0xFFc0eef3);
+const gold = Color(0xffeddb9e);
+
+/// Dark Mode Colors
+/// 0a1d26 - nice dark blue
+const bgColorDarkMode = Color(0xff1a1a1a);
+const bgColorDarkModeAlt = Color(0xff242525);
+
+const textWhite900 = Color(0xFFFFFFFF);
+const textWhite700 = Color(0xFFE5E5E5);
+const textWhite500 = Color(0xFFCECECE);
+const textWhite300 = Color(0xFFB9B9B9);
+const textWhite100 = Color(0xFFA0A0A0);
---
+++ lib/theme/my_theme.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'colors.dart';
+
+class MyTheme {
+  /// Dark Theme
+  ThemeData get darkTheme => ThemeData(
+      primaryColor: accent,
+      primaryColorDark: bgColorDarkModeAlt,
+      scaffoldBackgroundColor: bgColorDarkMode,
+      iconTheme: const IconThemeData(color: textWhite300),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: accent, foregroundColor: textWhite700),
+      textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+              foregroundColor: accent, backgroundColor: Colors.transparent)),
+      filledButtonTheme: FilledButtonThemeData(
+          style: FilledButton.styleFrom(
+              backgroundColor: accent, foregroundColor: textWhite500)),
+      textSelectionTheme: const TextSelectionThemeData(cursorColor: accent),
+      inputDecorationTheme: const InputDecorationTheme(
+          iconColor: accent,
+          prefixIconColor: textWhite300,
+          suffixIconColor: textWhite300,
+          focusedBorder:
+              UnderlineInputBorder(borderSide: BorderSide(color: accent))),
+      bottomSheetTheme: const BottomSheetThemeData(
+        backgroundColor: bgColorDarkMode,
+        elevation: 0,
+        dragHandleColor: textWhite500,
+        showDragHandle: true,
+        surfaceTintColor: bgColorDarkMode,
+        modalBackgroundColor: bgColorDarkMode,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(24), topRight: Radius.circular(24))),
+      ),
+      useMaterial3: true,
+      fontFamily: 'DMSans',
+      textTheme: const TextTheme(
+        bodySmall: TextStyle(
+            fontSize: 11, fontWeight: FontWeight.w500, color: textWhite500),
+        bodyMedium: TextStyle(
+            fontSize: 14, fontWeight: FontWeight.w500, color: textWhite500),
+        bodyLarge: TextStyle(
+            fontSize: 16, fontWeight: FontWeight.w500, color: textWhite700),
+        titleSmall: TextStyle(
+            fontSize: 18, fontWeight: FontWeight.w700, color: textWhite700),
+        titleMedium: TextStyle(
+            fontSize: 20, fontWeight: FontWeight.bold, color: textWhite900),
+        titleLarge: TextStyle(
+            fontSize: 26, fontWeight: FontWeight.bold, color: textWhite900),
+      ));
+}
