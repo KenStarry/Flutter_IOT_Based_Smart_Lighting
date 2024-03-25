be22060c8bb4bd8ccf01f4aa6b6af0b9e9d085d3
KenStarry <starrycodes@gmail.com>
2024-03-25 12:36:08 +0300

    LDR toggles

--- lib/feature_home/presentation/home_page.dart
+++ lib/feature_home/presentation/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,9 +11,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: Text("Hello"),
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle(
+          systemNavigationBarColor: Theme.of(context).scaffoldBackgroundColor,
+          systemNavigationBarIconBrightness: Brightness.light,
+          statusBarBrightness: Brightness.light,
+          statusBarColor: Theme.of(context).scaffoldBackgroundColor),
+      child: Scaffold(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        body: SafeArea(
+            child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Theme.of(context).scaffoldBackgroundColor,
+              child: Column(
+                children: [
+                  //  toggles
+
+                  //  light bulb and controls
+
+                  //  slider value
+                ],
+              ),
+        )),
+      ),
     );
   }
 }
