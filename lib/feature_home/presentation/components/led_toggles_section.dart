47e7cbb5d2004c225dc0b4087d009f022e4ee16a
KenStarry <starrycodes@gmail.com>
2024-03-25 13:04:35 +0300

    Active Led Toggle

--- lib/feature_home/presentation/components/led_toggles_section.dart
+++ lib/feature_home/presentation/components/led_toggles_section.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:smart_lighting/feature_home/presentation/components/led_pill.dart';
+import 'package:smart_lighting/feature_home/presentation/controller/home_controller.dart';
 
 class LedTogglesSection extends StatefulWidget {
   const LedTogglesSection({super.key});
@@ -9,12 +11,15 @@ class LedTogglesSection extends StatefulWidget {
 }
 
 class _LedTogglesSectionState extends State<LedTogglesSection> {
+  late final HomeController _homeController;
   late final List<String> leds;
 
   @override
   void initState() {
     super.initState();
     leds = ["Led 1", "Led 2", "Led 3"];
+
+    _homeController = Get.find<HomeController>();
   }
 
   @override
@@ -27,7 +32,14 @@ class _LedTogglesSectionState extends State<LedTogglesSection> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: leds
-            .map((led) => LedPill(title: led, isActive: false, onTap: () {}))
+            .map((led) => Obx(
+                  () => LedPill(
+                      title: led,
+                      isActive: _homeController.activeLed.value == led,
+                      onTap: () {
+                        _homeController.setActiveLed(led: led);
+                      }),
+                ))
             .toList(),
       ),
     );
