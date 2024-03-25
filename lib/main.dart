510465326b5c4778bcfd7848cc30d5f820a71d11
KenStarry <starrycodes@gmail.com>
2024-03-25 15:46:06 +0300

    Firebase cloud firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:smart_lighting/feature_home/di/controllers_di.dart';
 import 'package:smart_lighting/feature_home/presentation/home_page.dart';
+import 'package:smart_lighting/firebase_options.dart';
 import 'package:smart_lighting/theme/my_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   invokeControllers();
 
@@ -24,4 +28,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
