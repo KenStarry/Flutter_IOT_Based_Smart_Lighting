b11cb8c8d16e5d72c40d12a593af071e6f9dc0e2
KenStarry <starrycodes@gmail.com>
2024-03-25 12:39:40 +0300

    Lamp hanger rope

---
+++ lib/feature_home/presentation/components/lamp_hanger_rope.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LampHangerRope extends StatelessWidget {
+  final double screenWidth, screenHeight;
+  final Color color;
+
+  const LampHangerRope(
+      {super.key,
+      required this.screenWidth,
+      required this.screenHeight,
+      required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: screenWidth * 0.2,
+      child: Container(
+        color: color,
+        width: 15,
+        height: screenHeight * 0.15,
+      ),
+    );
+  }
+}
