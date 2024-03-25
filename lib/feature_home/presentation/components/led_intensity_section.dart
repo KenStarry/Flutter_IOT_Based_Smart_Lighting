dbeeb54512899f7c42e4cfa44e3035f5feda84d5
KenStarry <starrycodes@gmail.com>
2024-03-25 15:08:02 +0300

    Set individual values for the low, medium and high values

--- lib/feature_home/presentation/components/led_intensity_section.dart
+++ lib/feature_home/presentation/components/led_intensity_section.dart
@@ -25,120 +25,134 @@ class _LedIntensitySectionState extends State<LedIntensitySection> {
 
   @override
   Widget build(BuildContext context) {
-    return SleekCircularSlider(
-      min: 0.0,
-      max: 255.0,
-      initialValue: 255.0,
-      appearance: CircularSliderAppearance(
-          size: 300,
-          animationEnabled: true,
-          startAngle: 180,
-          angleRange: 180,
-          customWidths: CustomSliderWidths(
-              trackWidth: 8, progressBarWidth: 8, handlerSize: 8),
-          customColors: CustomSliderColors(
-              trackColor: Theme.of(context).scaffoldBackgroundColor,
-              progressBarColors: [
-                textWhite700,
-                gold,
-              ],
-              hideShadow: true,
-              dynamicGradient: true,
-              dotColor: textWhite700)),
-      onChange: (value) {
-        print("VALUEEE : ${value}");
-      },
-      innerWidget: (value) => Container(
-        padding: const EdgeInsets.all(8),
-        margin: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-            color: Theme.of(context).scaffoldBackgroundColor,
-            shape: BoxShape.circle),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(height: 32),
-            Text('Intensity', style: Theme.of(context).textTheme.bodySmall),
+    return Obx(
+      () => SleekCircularSlider(
+        min: 0.0,
+        max: 255.0,
+        initialValue: _homeController.activeLedMode.value == "Low"
+            ? _homeController.ledLowLevel.value
+            : _homeController.activeLedMode.value == "Medium"
+                ? _homeController.ledMediumLevel.value
+                : _homeController.ledHighLevel.value,
+        appearance: CircularSliderAppearance(
+            size: 300,
+            animationEnabled: true,
+            startAngle: 180,
+            angleRange: 180,
+            customWidths: CustomSliderWidths(
+                trackWidth: 8, progressBarWidth: 8, handlerSize: 8),
+            customColors: CustomSliderColors(
+                trackColor: Theme.of(context).primaryColorDark,
+                progressBarColors: [
+                  textWhite700,
+                  gold,
+                ],
+                hideShadow: true,
+                dynamicGradient: true,
+                dotColor: textWhite700)),
+        onChange: (value) {
+          if (_homeController.activeLedMode.value == "Low") {
+            _homeController.setIntensityLevels(low: value);
+          } else if (_homeController.activeLedMode.value == "Medium") {
+            _homeController.setIntensityLevels(medium: value);
+          } else if (_homeController.activeLedMode.value == "High") {
+            _homeController.setIntensityLevels(high: value);
+          }
+        },
+        innerWidget: (value) => Container(
+          padding: const EdgeInsets.all(8),
+          margin: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              color: Theme.of(context).scaffoldBackgroundColor,
+              shape: BoxShape.circle),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 32),
+              Text('Intensity', style: Theme.of(context).textTheme.bodySmall),
 
-            const SizedBox(height: 24),
+              const SizedBox(height: 24),
 
-            //  controls
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                //  minus
-                GestureDetector(
-                  onTap: (){
-                    //  subtract overall value from either low, medium or high
-                  },
-                  child: Container(
-                    width: 50,
-                    height: 50,
+              //  controls
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //  minus
+                  GestureDetector(
+                    onTap: () {
+                      //  subtract overall value from either low, medium or high
+                    },
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                          border: Border.all(
+                            color: Colors.white.withOpacity(0.2),
+                            width: 1,
+                          )),
+                      child: const Center(
+                        child: Icon(
+                          Icons.remove_rounded,
+                          color: textWhite500,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  //  percentage icon
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 12),
                     decoration: BoxDecoration(
-                        shape: BoxShape.circle,
+                        borderRadius: BorderRadius.circular(150),
                         color: Theme.of(context).scaffoldBackgroundColor,
                         border: Border.all(
                           color: Colors.white.withOpacity(0.2),
                           width: 1,
                         )),
-                    child: const Center(
-                      child: Icon(
-                        Icons.remove_rounded,
-                        color: textWhite500,
+                    child: Text('${value.ceil()}',
+                        style: TextStyle(
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .titleLarge!
+                                .fontSize,
+                            fontWeight: Theme.of(context)
+                                .textTheme
+                                .titleLarge!
+                                .fontWeight,
+                            color: Theme.of(context).primaryColor)),
+                  ),
+
+                  // add icon
+                  GestureDetector(
+                    onTap: () {
+                      //  add overall value from either low, medium or high
+                    },
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                          border: Border.all(
+                            color: Colors.white.withOpacity(0.2),
+                            width: 1,
+                          )),
+                      child: const Center(
+                        child: Icon(
+                          Icons.add_rounded,
+                          color: textWhite500,
+                        ),
                       ),
                     ),
                   ),
-                ),
-
-                //  percentage icon
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(150),
-                      color: Theme.of(context).scaffoldBackgroundColor,
-                      border: Border.all(
-                        color: Colors.white.withOpacity(0.2),
-                        width: 1,
-                      )),
-                  child: Text('65%',
-                      style: TextStyle(
-                          fontSize:
-                              Theme.of(context).textTheme.titleLarge!.fontSize,
-                          fontWeight: Theme.of(context)
-                              .textTheme
-                              .titleLarge!
-                              .fontWeight,
-                          color: Theme.of(context).primaryColor)),
-                ),
-
-                // add icon
-                GestureDetector(
-                  onTap: (){
-                    //  add overall value from either low, medium or high
-                  },
-                  child: Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Theme.of(context).scaffoldBackgroundColor,
-                        border: Border.all(
-                          color: Colors.white.withOpacity(0.2),
-                          width: 1,
-                        )),
-                    child: const Center(
-                      child: Icon(
-                        Icons.add_rounded,
-                        color: textWhite500,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )
-          ],
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
